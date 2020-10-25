a4b4b4f93fa50b5b67c075256610fbbb6a51fd83
DJ <social.deej@gmail.com>
2020-10-25 13:22:35 +0530

    Able to link up different DBs

--- server-2.0/routers/computation.js
+++ server-2.0/routers/computation.js
@@ -11,7 +11,6 @@ router.get('/assignservice/:id', async(req, res) => {
     var id = req.params.id 
     try{
         const user = await User.find({_id: id})
-        const ngo = await NGO.find({})
         console.log(user[0].location)
 
         var worker = await Worker.find({source: user[0].location})
@@ -19,6 +18,7 @@ router.get('/assignservice/:id', async(req, res) => {
         user[0].personincharge = worker[0]._id
         
         await user[0].populate('personincharge').execPopulate()
+        await user[0].save();
         console.log(user);
         res.send(user)
     } catch (e){
@@ -27,8 +27,33 @@ router.get('/assignservice/:id', async(req, res) => {
     }
 })
 
-router.get('/getaccountsforngo/:id', async (req, res) => {
-    
+router.post('/postparcelsincollections', info,async (req, res) => {
+    const reviews = req.body.comments;
+    try {
+        const user = await User.findById(req.user._id)
+        var ngo = await NGO.findOne({name: user.ngoChosen})
+        var template = {userinfo : req.user._id, comments: reviews}
+        ngo.comments.push(template)
+        await ngo.populate('comments.userinfo').execPopulate()
+        await ngo.save();
+        res.send(ngo);
+    } catch (e){
+        console.log(e);
+        res.send(e);
+    }
 })
 
 module.exports = router;
+
+
+/*
+const ngo = await NGO.findById(id);
+        const user = await User.find({_id: '5f94829db0e091fce25de714'})
+        user[0].ngoChosen = 'ANOKHA NGO'
+        console.log(user[0].ngoChosen)
+        var template = { userinfo : '5f94829db0e091fce25de714', comments: 'Hello World -1'}
+        ngo.comments.push(template)
+        await ngo.populate('comments.userinfo').execPopulate()
+        console.log(ngo)
+        res.send(ngo);
+*/
